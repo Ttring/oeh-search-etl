457372483a6e96f5de29896d45282993b5e15c7e
Yap <lei-cheng.yap@capgemini.com>
2022-06-28 17:26:27 +0200

    add merge sort

--- find_duplicate.py
+++ find_duplicate.py
@@ -23,6 +23,10 @@ class find_duplicate:
         self.client = pymongo.MongoClient(self.mongo_uri)
         self.db = self.client[self.mongo_db]
 
+        # key_list for sorting
+        self.key_list = []
+        self.copy_key_list = []
+
     def create_sort_key(self):
         # filter off collection name "system.version" and save the rest as self.collection
         self.collection = self.db.list_collection_names(filter={'type': 'collection', 'name': {'$ne': 'system.version'}})
@@ -34,16 +38,31 @@ class find_duplicate:
             # list every document in collection
             for document in self.db[collection].find():
                 #pprint(data)
+                try: 
+                    size = document['lom']['technical']['duration']
+                except KeyError:
+                    size = document['lom']['technical']['size']
+
                 # change title to small capital letters and combine it with it's format type
-                key = self.hashing(document['lom']['general']['title'].lower()) + self.get_non_vowels(document['lom']['technical']['format'])
-                # insert new field into document
+                key = self.get_hashed_title(document['lom']['general']['title'].lower()) + self.get_non_vowels(document['lom']['technical']['format']) + self.get_format_size(size)
+
+                # append key into key_list for merge sort
+                self.key_list.append(key)
+
+                # insert key and collection as field into document
                 self.db[collection].update_one( {"_id": document["_id"]}, {"$set": {"sortKey": key}})
-        
-    def hashing(self, title):
-        # hash the string for comparison purposes
+                self.db[collection].update_one( {"_id": document["_id"]}, {"$set": {"spiderName": collection}})
+
+        self.copy_key_list = list(self.key_list)
+        self.merge_sort(self.copy_key_list, self.key_list, 0, len(self.key_list))
+        print(self.key_list)
+
+                   
+    def get_hashed_title(self, title):
+        # hash the title for comparison purposes
         encoded_str = title.encode()
         hash_obj_sha224 = hashlib.sha224(encoded_str)
-        return hash_obj_sha224.hexdigest()
+        return hash_obj_sha224.hexdigest()[0:4]
     
     # convert MIME type to part of the key by getting it's non vowel alphabet such as application/pdf will be pplpdf
     def get_non_vowels(self, format):
@@ -64,7 +83,38 @@ class find_duplicate:
                     n += 1
             non_vowels += word[0:3]
         return non_vowels[0:4]
+    
+    def get_format_size(self, size): 
+        return str(size)[0:2]
 
+    def merge_sort(self, key_list, key_list_final, start, end):
+         if end - start < 2:
+             return
+         if end - start == 2:
+             if key_list_final[start] > key_list_final[start+1]:
+                 key_list_final[start],key_list_final[start+1] = key_list_final[start+1],key_list_final[start]
+             return
+         mid = (end + start) // 2
+         self.merge_sort(key_list_final, key_list, start, mid)
+         self.merge_sort (key_list_final, key_list, mid, end)
+           
+         i = start
+         j = mid
+         idx = start
+         while idx < end:
+             if j >= end or ( i < mid and key_list[i] < key_list[j]):
+                 key_list_final[idx] = key_list[i]
+                 i += 1
+             else:
+                 key_list_final[idx] = key_list[j]
+                 j += 1
+             idx += 1
 
 a = find_duplicate()
 a.create_sort_key()
+
+# b = [54,26,93,17,77,31,44,55,20]
+# copylist = list(b)
+# result = a.merge_sort(copylist, b, 0 , len(b))
+# print(copylist)
+# print(b)
