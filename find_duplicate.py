bba11f9bce7763f5385c81223db115c14e6ffd50
Yap <lei-cheng.yap@capgemini.com>
2022-07-26 16:51:50 +0200

    add sorted block

--- find_duplicate.py
+++ find_duplicate.py
@@ -16,6 +16,7 @@ from converter import settings
 3: sort metadata according to key
 4: create partition according to key
 5: implement sorted block algorithms 
+6: delete duplicate data based on sort key
 '''
 
 class FindDuplicate: 
@@ -31,19 +32,20 @@ class FindDuplicate:
 
         # key_list for sorting
         self.key_list_qh = [] # list for quick sort hybrid
+        self.first_par_element = [] #first element in partition
 
     def create_sort_key(self):
         # filter off collection name "system.version" and save the rest as self.collection
         self.collection = self.db.list_collection_names(filter={'type': 'collection', 'name': {'$ne':  'system.version'}})
         # list every collection 
         for collection in self.collection: 
-            if (collection == "sodix_spider"  or  collection =="test_spider"):
+            if (collection == "sodix_spider"  or  collection =="test_spider" or collection =="copy1sodix_spider"):
                 print("Collection name: " +collection) 
                 n = 0   
                 # list every document in collection
                 for document in self.db[collection].find():
                     #pprint(data)
-                    if n  == 5:
+                    if n  == 20:
                         break
                     try: 
                         size = document['lom']['technical']['duration']
@@ -75,7 +77,10 @@ class FindDuplicate:
         print("num\tvor Sortierung\tnach Sortierung")
         for i, (a, b) in enumerate(zip(copy_key_list, self.key_list_qh)):
             print(str(i) + '\t' +  a+ '\t' + b)
-        self.create_partition(self.key_list_qh)
+        p = list(self.create_partition(self.key_list_qh))
+        print(list(self.create_partition(self.key_list_qh)))
+        self.partition_size(p)
+        print(self.sorted_neighbourhood(self.key_list_qh,4))
 
     def hashed_title(self, title):
         # hash the title for comparison purposes
@@ -142,16 +147,68 @@ class FindDuplicate:
 
     def create_partition(self, list):
         start_p = 0
-        for i, sortkey in enumerate (list[:-1]):
-            if sortkey == list[i+1]:
+        end_p = 0
+        for i in range(len(list[:-1])): 
+            if(list[i][0:3] == list[i+1][0:3]) :
                 end_p = i + 1
-            else :
+            else:    
+                #print("start: " +str(start_p) +"\tend :" +str(end_p)) 
+                self.first_par_element.append(start_p) # save first element in partition
+                yield list[start_p : end_p +1] 
                 start_p = i + 1 
                 end_p = start_p
-            print("start: " +str(start_p) +"\tend :" +str(end_p)) if (end_p is not start_p) else ""
-        #for i in range(0, len(list), 2):
-         #   yield list[i : i+2]
+        self.first_par_element.append(start_p)
+        yield list[start_p : end_p +1]     
 
+    def partition_size(self, list):
+        self.max_partition_size = 0
 
+        for i in list: 
+            if(len(i)>self.max_partition_size):
+                self.max_partition_size = len(i)
+
+    def sorted_neighbourhood(self,sorted_list,o):
+        lcr = [] # list comparison records
+        window_num = o +1 # num of window in overlapping area
+        i = 0
+
+        # iterate over all records to search for duplicates
+        while i<len(sorted_list):
+            # record is the first element of new partition
+            #print("\nlcr:" , lcr)
+           # print("currently i :" +str(i) +" window_num :" , window_num)
+            if (i in self.first_par_element and i>0) or (len(lcr) == self.max_partition_size): 
+                # remove all records of the previous partition that is not in overlap
+                #print("element is the same as first element in partition")
+                while len(lcr) > o:
+                    #print("remove previous record in lcr")
+                    lcr.pop(0)
+                    #print("lcr:" , lcr)
+                window_num = 0
+            elif window_num <= o :
+                #print("if windown is less than 0")
+                lcr.pop(0)
+                #print("lcr:" , lcr)
+                window_num += 1
+            
+            # compare current record with all records in lcr
+            for j in range(len(lcr)): # print j from 1 onwards
+                #print("i:" ,i)
+                #print("sorted_list :" ,sorted_list) 
+                #print("lcr :" , lcr)
+               
+                if (sorted_list[i]== lcr[j]) and sorted_list.count(lcr[j])>1:
+                    sorted_list.pop(i)
+                    i-=1
+                    #print("sorted list after popping 1 record out :\n", sorted_list)               
+                else:         
+                    continue
+            
+            if len(lcr) == self.max_partition_size:
+                lcr.pop(0)
+
+            lcr.append(sorted_list[i])
+            i += 1 
+        return sorted_list
 a = FindDuplicate()
 a.create_sort_key()
